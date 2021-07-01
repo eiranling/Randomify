e32f590f6283e2e142f18a84d93b7b2bc18b80f3
Eiran Ling <eiranling@gmail.com>
2021-07-02 00:14:27 +1200

    Add .gitignore

--- src/components/artist-search-field/artist-search-field.tsx
+++ src/components/artist-search-field/artist-search-field.tsx
@@ -4,7 +4,7 @@ export default class ArtistSearchField extends React.Component {
     render() {
         return (
             <div className="randomify__artist-search">
-                <input type="text"></input>
+                
             </div>
         );
     }
